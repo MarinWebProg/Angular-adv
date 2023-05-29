fef69fdb79dad237427c1414edd759b1742ac6f3
sergio.marin <sergio.marin@durango.gob.mx>
2023-05-29 13:56:26 -0600

    feat(Back-End): :sparkles: Creacion del primer modulo

--- 03- adminPro/src/app/app.module.ts
+++ 03- adminPro/src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 
 /** Modulos*/
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
 
+/** Componentes*/
+import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NoPageFoundComponent } from './pages/no-page-found/no-page-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { BreadCrumbsComponent } from './shared/bread-crumbs/bread-crumbs.component';
-import { SideBarComponent } from './shared/side-bar/side-bar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
+
+
 
 @NgModule({
   declarations: [
@@ -22,17 +19,12 @@ import { PagesComponent } from './pages/pages.component';
     LoginComponent,
     RegisterComponent,
     NoPageFoundComponent,
-    DashboardComponent,
-    BreadCrumbsComponent,
-    SideBarComponent,
-    HeaderComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
   ],
   bootstrap: [AppComponent]
 })
