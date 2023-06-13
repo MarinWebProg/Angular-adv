d9ac619190943ee2bfdbb01390a421c3e7b656fa
sergio.marin <sergio.marin@durango.gob.mx>
2023-06-13 14:14:38 -0600

    feat(Back-End): :truck: Rutas hijas -forChild()

--- 03- adminPro/src/app/app-routing.module.ts
+++ 03- adminPro/src/app/app-routing.module.ts
@@ -1,29 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 /** COMPONENTES */
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { PagesComponent } from './pages/pages.component';
 
 /** PAGINA NO ENCONTRADO 404*/
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
+/** Modulo de rutas (rutas hijas)*/
+import { pagesRoutingModule } from './pages/pages.routing';
+
 const routes: Routes=[
-  {
-    path: '',
-    component: PagesComponent,
-    children:[
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'grafica1', component: Grafica1Component},
-      {path: '',redirectTo:'/dashboard', pathMatch:'full'},
-    ],
-  }, //Es la ruta donde estaran las rutas protegidas
-
-
+ //Es la ruta donde estaran las rutas protegidas
 
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
@@ -35,7 +23,7 @@ const routes: Routes=[
 @NgModule({
   imports: [
     RouterModule.forRoot(routes), //El  RouterModule.forRoot(); -->para configurar las rutas principales de una aplicación, especificando qué componentes se mostrarán cuando los usuarios visiten diferentes direcciones en el sitio web.
-
+    pagesRoutingModule,
   ],
   exports:[RouterModule],
 })
