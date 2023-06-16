d27a54bdf959d27573e057b4438694b467322109
sergio.marin <sergio.marin@durango.gob.mx>
2023-06-16 11:31:21 -0600

    feat(Full-Stack): :sparkles: Atributos personalizados

--- 03- adminPro/src/app/pages/progress/progress.component.ts
+++ 03- adminPro/src/app/pages/progress/progress.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent {
+export class ProgressComponent  {
+  progreso: number = 45;
 
+  get getPorcentaje(){
+    return `${this.progreso}%`;
+  }
+
+  cambiaValor (valor: number){
+    if(this.progreso > 100)
+    {
+       this.progreso = 100;
+    }
+    if(this.progreso < 0 )
+    {
+       this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor
+  }
 }
