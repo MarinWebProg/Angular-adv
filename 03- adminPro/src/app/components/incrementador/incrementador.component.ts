89803bca191d3b1d2fc112d47f528229b59588b6
sergio.marin <sergio.marin@durango.gob.mx>
2023-07-10 10:00:44 -0600

    feat(Full-Stack): :sparkles: Color de los botones de forma condicional

--- 03- adminPro/src/app/components/incrementador/incrementador.component.ts
+++ 03- adminPro/src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,23 +6,33 @@ import { Component, Input } from '@angular/core';
   styles: [
   ]
 })
-export class IncrementadorComponent {
+export class IncrementadorComponent implements OnInit{
+  ngOnInit(){
+    this.btnClass = `btn ${this.btnClass}`;
+  }
 
   //@Input('valor') progreso: number = 45; // Dentro de parentecis ('' ) podemos ponerle el nombre del input como su identificador
-  @Input() progreso: number = 45;
+  @Input('valor') progreso: number = 45;
+  @Input() btnClass: string = 'btn btn-primary'
 
+  //@Output -Disparador de la accion del padre-Salida
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
 
 
   cambiaValor (valor: number){
-    if(this.progreso > 100)
-    {
-       this.progreso = 100;
+    if(this.progreso >= 100 && valor >=0){
+      this.valorSalida.emit(100);
+      return this.progreso = 100;
     }
-    if(this.progreso < 0 )
-    {
-       this.progreso = 0;
+
+    if(this.progreso <= 0 && valor < 0){
+      this.valorSalida.emit(0);
+      return this.progreso = 0;
     }
 
     this.progreso = this.progreso + valor
+    this.valorSalida.emit(this.progreso)
+
+    return this.cambiaValor;
   }
 }
